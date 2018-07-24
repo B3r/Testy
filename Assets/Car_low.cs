7afc3721187040ed835e0a8bf9da7db57738789d
Marvin Flock <Marvin.Flock@7p-group.com>
2018-07-24 20:09:05 +0200

    Playing with Speed

--- Assets/Car_low.cs
+++ Assets/Car_low.cs
@@ -1,31 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Car_low : MonoBehaviour {
     private Vector3 velocity, input, output;
     private Rigidbody rig;
     private bool isMoving;
     private readonly float DEGREES = 180f;
+    private float distLeft, distRight, currentSpeed;
+    private Vector3[] weights;
 
-	// Use this for initialization
-	void Start () {
+    public Vector3[] Weights
+    {
+        get
+        {
+            return weights;
+        }
+
+        set
+        {
+            weights = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        weights = new Vector3[5];
         rig = GetComponent<Rigidbody>();
-        rig.velocity = new Vector3(0, 0, -5);
+        rig.velocity = new Vector3(0, 0, -5f);
         isMoving = true;
         output = new Vector4(0.9f, 0.4f, 1f, 0);
-        UpdateRotation(output);
-        UpdateVelocity(output);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         if (isMoving)
         {
-            isMoving = false;
+            RaycastHit hitRight, hitLeft;
+
+            //get distance to right
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitRight, Mathf.Infinity))
+            {
+                distRight = hitRight.distance;
+            }
+            //get distance to left
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitLeft, Mathf.Infinity))
+            {
+                distLeft = hitLeft.distance;
+            }
+            //get speed
+            float currentSpeed = rig.velocity.magnitude;
+
+            // neuronal here
+            //output = calcOutputNeurons(distLeft,distRight,currentSpeed);
+            //UpdateRotation(output);
+            //UpdateVelocity(output);
         }
 	}
 
+    //turn left or right based on 180 degrees
     void UpdateRotation(Vector4 output)
     {
         float angle = (output[0] - output[1]) * DEGREES;
