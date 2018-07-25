dfddce25266a4dfa496eb43d0c5329251842c85b
Marvin Flock <Marvin.Flock@7p-group.com>
2018-07-25 18:07:34 +0200

    Restarting Rounds

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
+    private bool isStopped = true;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        isStopped = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        // if all cars stopped, restart round
-        RestartRound();
+        if (isStopped)
+        {
+            // if all cars stopped, restart round
+            RestartRound();
+            return;
+        }
 
-
+        //calculate each second
 	}
 
     private void RestartRound()
     {
-        throw new NotImplementedException();
+        //destroy cars
+        //create new cars
+        isStopped = false; // start round again
     }
 }
