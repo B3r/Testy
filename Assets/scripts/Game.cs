c10286c60e1fce9a247a8b9086c5595579de5bdc
Marvin Flock <Marvin.Flock@7p-group.com>
2018-07-26 19:12:21 +0200

    Changed prefab behavior

--- Assets/scripts/Game.cs
+++ Assets/scripts/Game.cs
@@ -1,21 +1,46 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Game : MonoBehaviour {
 
     private bool isStopped = true;
-
-
+    private readonly int carsPerCat = 5;
+    private readonly int categoriesCount = 4;
+    private string[] categories = new string[4];
+    private readonly Vector3 startingPos = new Vector3(2.5f,0.25f,0);
+    public Transform car_prefab;
+    private Transform[][] cars;
 	// Use this for initialization
-	void Start () {
-        isStopped = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void Start ()
+    {
+        isStopped = true;
+        categories[0] = "red";
+        categories[1] = "yellow";
+        categories[2] = "green";
+        categories[3] = "blue";
 
+        // init Cars
+        initCars(categories);
+    }
+
+    private void initCars(string[] categories)
+    {
+        cars = new Transform[categoriesCount][];
+        for (int x = 0; x < categoriesCount; x++)
+        {
+            cars[x] = new Transform[carsPerCat];
+            for (int y = 0; y < carsPerCat; y++)
+            {
+                Transform car = cars[0][y] = Instantiate(car_prefab, startingPos, Quaternion.identity);
+                car.name = categories[x] + "car" + y;
+                // color here
+                cars[0][y] = car;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
         if (isStopped)
         {
             // if all cars stopped, restart round
@@ -30,6 +55,7 @@ public class Game : MonoBehaviour {
     {
         //destroy cars
         //create new cars
+        
         isStopped = false; // start round again
     }
 }
