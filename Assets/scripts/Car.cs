0b5ae2653923ca6d7052cf6813a47ae8965309f7
MF <Marvin.Flock@rwth-aachen.de>
2018-07-29 21:50:20 +0200

    Need to fix rotation + forward movement

--- Assets/scripts/Car.cs
+++ Assets/scripts/Car.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Car : MonoBehaviour {
     private Rigidbody rig;
-    private readonly float DEGREES = 180f;
+    private readonly float DEGREES = 90f;
+    private Vector3 lastPosition;
+
+    private readonly int transitions = 3;
+    private readonly int[] neuronsPerLayers = { 3, 5, 4, 4 };
 
     public float DistLeft { get; set; }
 
@@ -12,27 +17,57 @@ public class Car : MonoBehaviour {
 
     public bool IsMoving { get; set; }
 
+    public double[][][] Weights { get; set; }
+
+    public float TimeRunning { get; set; }
+
+    public float DistanceTravelled { get; set; }
+
     // Use this for initialization
     void Start () {
         rig = GetComponent<Rigidbody>();
-        rig.velocity = new Vector3(0f, 0f, 0f);
+        rig.velocity = Vector3.left;
+        InitWeights();
+        TimeRunning = 0;
+        DistanceTravelled = 0;
+        lastPosition = transform.position;
         IsMoving = true;
     }
 
+    private void InitWeights()
+    {
+        Weights = new double[transitions][][];
+        for (int x = 0; x < transitions; x++) // for each transition
+        {
+            Weights[x] = new double[neuronsPerLayers[x + 1]][];
+            for (int y = 0; y < Weights[x].Length; y++) // for each outputNeuron
+            {
+                Weights[x][y] = new double[neuronsPerLayers[x]]; //set an inputNeuron Vector
+                for (int z = 0; z < Weights[x][y].Length; z++)
+                {
+                    // fill initial weights with ones
+                    Weights[x][y][z] = UnityEngine.Random.Range(0.0f, 1.0f);
+                }
+            }
+        }
+    }
     // Update is called once per frame
     void FixedUpdate () {
         if (IsMoving)
         {
+            DistanceTravelled += Vector3.Distance(transform.position, lastPosition);
+            lastPosition = transform.position;
+            TimeRunning += Time.deltaTime;
             RaycastHit hitRight, hitLeft;
 
             //get distance to right
             // Does the ray intersect any objects excluding the player layer
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitRight, Mathf.Infinity))
+            if (Physics.Raycast(transform.position, transform.TransformDirection(transform.forward), out hitRight, Mathf.Infinity))
             {
                 DistRight = hitRight.distance;
             }
             //get distance to left
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitLeft, Mathf.Infinity))
+            if (Physics.Raycast(transform.position, transform.TransformDirection(transform.forward), out hitLeft, Mathf.Infinity))
             {
                 DistLeft = hitLeft.distance;
             }
@@ -41,24 +76,41 @@ public class Car : MonoBehaviour {
             if(CurrentSpeed == 0)
             {
                 IsMoving = false;
+                return;
             }
+            double[] inputVector = {DistLeft,DistRight,CurrentSpeed};
+            double[] outputVector = {0,0,0,0};
+            // call sigmoid for each transition
+            for(int i = 0; i < 3; i++)
+            {
+                outputVector = Sigmoid(Weights[i],inputVector);
+                inputVector = outputVector;
+            }
+            UpdateRotation(outputVector);
+            UpdateVelocity(outputVector);
         }
 	}
 
     //turn left or right based on 180 degrees
-    public void UpdateRotation(Vector4 output)
+    public void UpdateRotation(double[] output)
     {
-        float angle = (output[0] - output[1]) * DEGREES;
+        float angle = (float)(output[0] - output[1]) * DEGREES;
+        // Rotate around y - axis
         transform.Rotate(Vector3.up, angle);
     }
 
-    public void UpdateVelocity(Vector4 output)
+    public void UpdateVelocity(double[] output)
     {
-        float deltaSpeed = (output[2] - output[3]) * Time.deltaTime;
-        float angle = rig.rotation.y * DEGREES;
-        float resultX = (CurrentSpeed + deltaSpeed) * Mathf.Sin(angle);
-        float resultZ = (CurrentSpeed + deltaSpeed) * Mathf.Cos(angle);
-        rig.velocity = new Vector3(resultX, 0, resultZ);
+        float deltaSpeed = (float)(output[2] - output[3]) * Time.deltaTime;
+        CharacterController controller = GetComponent<CharacterController>();
+
+        // Move forward / backward
+        controller.SimpleMove(transform.forward * deltaSpeed);
+
+        //float angle = rig.rotation.y * DEGREES;
+        //float resultX = (CurrentSpeed + deltaSpeed) * Mathf.Sin(angle);
+        //float resultZ = (CurrentSpeed + deltaSpeed) * Mathf.Cos(angle);
+        //rig.velocity = new Vector3(resultX, 0, resultZ);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -67,6 +119,7 @@ public class Car : MonoBehaviour {
         {
             Stop();
             IsMoving = false;
+            Debug.Log("Car collided!!!");
         }
     }
 
@@ -75,7 +128,21 @@ public class Car : MonoBehaviour {
         rig.velocity = Vector3.zero;
         rig.angularVelocity = Vector3.zero;
         rig.isKinematic = true;
-        rig.Sleep();
     }
 
+    private double[] Sigmoid(double[][] weights, double[] inputNeurons)
+    {
+        double[] outputNeurons = new double[weights.Length];
+        double neuronValue;
+        for (int i = 0; i < weights.Length; i++) // for each layer
+        {
+            neuronValue = 0f;
+            for (int j = 0; j < weights[i].Length; j++) // for each input
+            {
+                neuronValue += weights[i][j] * inputNeurons[j];
+            }
+            outputNeurons[i] = 1 / (1 + Math.Exp(neuronValue));
+        }
+        return outputNeurons;
+    }
 }
--- Assets/scripts/Game.cs
+++ Assets/scripts/Game.cs
@@ -7,15 +7,14 @@ public class Game : MonoBehaviour {
 
     private readonly int carsPerCat = 5;
     private readonly int categoriesCount = 4;
-    private readonly int transitions = 3;
-    private readonly int[] neuronsPerLayers = {3,5,4,4};
+    private float totalTime, roundTime;
     private int frames = 0;
-  
-        
-    private string[] categories = { "red", "yellow", "green", "blue" };
-    private Color[] colors = { Color.red, Color.yellow, Color.green, Color.blue };
-    private double[][][][] globalWeights = new double[4][][][]; //weights per category
+
+    private readonly string[] categories = { "red", "yellow", "green", "blue" };
+    private readonly Color[] colors = { Color.red, Color.yellow, Color.green, Color.blue };
     private readonly Vector3 startingPos = new Vector3(2.5f,0.25f,0);
+
+    private double[][][] bestWeights;
 
     public Transform car_prefab;
     private Transform[][] cars;
@@ -23,29 +22,11 @@ public class Game : MonoBehaviour {
     // Use this for initialization
 	void Start ()
     {
+        totalTime = 0f;
+        roundTime = 0f;
         // init Cars
-        InitWeights();
         InitCars();
         isAnyMoving = true;
-    }
-
-    private void InitWeights()
-    {
-        for (int x = 0; x < globalWeights.Length; x++) // for each category
-        {
-            globalWeights[x] = new double[transitions][][];
-            for(int y = 0; y < transitions; y++) // for each transition
-            {
-                globalWeights[x][y] = new double[neuronsPerLayers[y+1]][];
-                for (int z = 0; z < globalWeights[x][y].Length; z++) // for each outputNeuron
-                {
-                    globalWeights[x][y][z] = new double[neuronsPerLayers[y]]; //set an inputNeuron Vector
-
-
-                    // TODO: fill initial weights
-                }
-            }
-        }
     }
 
     private void InitCars()
@@ -66,80 +47,76 @@ public class Game : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate () {
-        if (!isAnyMoving)
+        Debug.Log(roundTime);
+        if (!isAnyMoving || roundTime > 60)
         {
             RestartRound();
             return;
         }
 
+        roundTime += Time.deltaTime;
+        totalTime += Time.deltaTime;
+
         frames++;
         if(frames >= 100)
         {
+            isAnyMoving = false;
             frames = 0;
-        
             //calculate each second
             for (int x = 0; x < categoriesCount; x++)
             {
                 for (int y = 0; y < carsPerCat; y++)
                 {
-                    Vector4 sampleOutput = CalculateOutputNeuronsForCar(cars[x][y]);
-                
                     Car currentCar = cars[x][y].GetComponent<Car>();
-                    
                     if (currentCar.IsMoving)
                     {
                         isAnyMoving = true;
-                        currentCar.UpdateRotation(sampleOutput);
-                        currentCar.UpdateVelocity(sampleOutput);
                     }
                 }
             }
         }
     }
 
-    private Vector4 CalculateOutputNeuronsForCar(Transform transform)
-    {
-        Car car = transform.gameObject.GetComponent<Car>();
-        double[] inputNeurons = {car.DistLeft,car.DistRight,car.CurrentSpeed};
-        
-        double[][] weights; //TODO calc weights by strategy
-        double[] outputNeurons = Sigmoid(weights, inputNeurons);
-    }
-
-    private double[] Sigmoid(double[][] weights, double[] inputNeurons)
-    {
-        double[] outputNeurons = new double[weights.Length];
-        double neuronValue;
-        for(int i = 0; i < weights.Length; i++) // for each layer
-        {
-            neuronValue = 0f;
-            for (int j = 0; j < weights[i].Length; j++) // for each input
-            {
-                neuronValue += weights[i][j] * inputNeurons[j];
-            }
-            outputNeurons[i] = 1 / (1 + Math.Exp(neuronValue));
-
-        }
-        return outputNeurons;
-    }
-
     private void RestartRound()
     {
-
-        //destroy cars
+        //get best car!
+        float bestCar = -1f;
+        //stop all cars and reset!
         for (int x = 0; x < categoriesCount; x++)
         {
             for (int y = 0; y < carsPerCat; y++)
             {
-                Destroy(cars[x][y].gameObject);
+                cars[x][y].GetComponent<Rigidbody>().isKinematic = false;
+                cars[x][y].GetComponent<Rigidbody>().velocity = Vector3.left;
+                cars[x][y].transform.position = startingPos;
+                cars[x][y].transform.rotation = Quaternion.identity;
+                Car car = cars[x][y].GetComponent<Car>();
+
+                // most successfull car: time travelled * distance travelled
+                float currentCarSuccess = car.TimeRunning * car.DistanceTravelled;
+                if(currentCarSuccess > bestCar)
+                {
+                    bestWeights = car.Weights;
+                    bestCar = currentCarSuccess;
+                }
+                car.TimeRunning = 0;
+                car.DistanceTravelled = 0;
             }
         }
 
-        //create new cars
-        InitCars();
+        // set new weights based on 
+        for (int x = 0; x < categoriesCount; x++)
+        {
+            for (int y = 0; y < carsPerCat; y++)
+            {
+                Car car = cars[x][y].GetComponent<Car>();
+                car.Weights = bestWeights;
+            }
+        }
 
         // start round again
         isAnyMoving = true;
         frames = 0;
+        roundTime = 0f;
     }
 }
